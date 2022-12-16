660514ad08f12a777d384113fc09b57c2cbfb5a6
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-12-16 16:44:42 +0200

    define and install requirments lib

---
+++ main.py
@@ -0,0 +1,15 @@
+# Used lib 
+import nltk
+nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+import numpy
+import tensorflow as tf
+import tflearn
+import random
+import json
+import pickle
+from time import sleep
+
+
+
+
